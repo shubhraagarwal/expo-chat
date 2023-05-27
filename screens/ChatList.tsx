9316a7b237a242ce7096e901d934045a1d1a4fc1
Shubhra Agarwal <shubhra@metaworkhq.com>
2023-05-28 00:29:25 +0530

    feat: displaying chatlist

--- screens/ChatList.tsx
+++ screens/ChatList.tsx
@@ -1,8 +1,17 @@
 import { View, Text, Pressable } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../hooks/useAuth";
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useNavigation } from "@react-navigation/native";
 
@@ -18,6 +27,38 @@ const ChatList = () => {
       }
     });
   };
+  const fetchChatList = async () => {
+    try {
+      const q = query(
+        collection(db, "users"),
+        where("email", "==", auth.currentUser?.email)
+      );
+      let chatList = [];
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        console.log(doc, "q");
+        chatList = doc.data().chats.reverse();
+      });
+      //   console.log(chatList, "chatList");
+      const uniqueChats = [];
+      const chatMap = new Map();
+
+      for (const chat of chatList) {
+        const chatWith = chat.chatWith;
+        if (!chatMap.has(chatWith)) {
+          chatMap.set(chatWith, chat);
+          uniqueChats.push(chat);
+        }
+      }
+      console.log(uniqueChats, "uniqueChats");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchChatList();
+  }, []);
   return (
     <View>
       <Text>Chat scList</Text>
@@ -30,6 +71,13 @@ const ChatList = () => {
         <Pressable onPress={() => fetchUsers()}>
           <Text>New Conversation</Text>
         </Pressable>
+        {users?.map((user, index) => (
+          <View key={index}>
+            <Pressable onPress={() => navigation.navigate("Chat", { user })}>
+              <Text>{user}</Text>
+            </Pressable>
+          </View>
+        ))}
         {users?.map((user, index) => (
           <View key={index}>
             <Pressable onPress={() => navigation.navigate("Chat", { user })}>
